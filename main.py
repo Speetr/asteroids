dea529ebbb5c751faf01cb175e0da24a3e2a5f84
Speetr <peetrsullivan@gmail.com>
2026-01-20 20:08:45 -0800

    capped fps to 60 and added comments

--- main.py
+++ main.py
@@ -4,14 +4,34 @@ from logger import log_state
 
 def main():
     pygame.init()
+
+    # set game resolution
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # used to set fps to 60
+    gameclock = pygame.time.Clock()
+    dt = 0
+
+    #  main game loop
     while True:
+        # write to our log
         log_state()
+
+        #get user input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #black background
         screen.fill("black")
+
+        # refresh the screen
         pygame.display.flip()
+
+        # pause the game until 1/60th of a second has passed
+        dt = gameclock.tick(60) / 1000
+        # print(dt)
+
     # print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     # print(f"Screen width: {SCREEN_WIDTH}")
     # print(f"Screen height: {SCREEN_HEIGHT}")
